10238ee96268202a6831ea15d01a978a32452e98
satnamsingh611 <125273094+satnamsingh611@users.noreply.github.com>
2023-10-04 08:08:29 +0530

    fixed persona error

--- src/utils/callPersona.js
+++ src/utils/callPersona.js
@@ -5,14 +5,12 @@ export const callPersona = async (prompt, userId) => {
   const hashId = btoa(String(userId).toLowerCase())
  
 
-  if(typeof hashId === null){
-    alert("Please login first")
-  }
   const response = await axios.post(api + 'generate', {
     prompt,
-    userId,
+    userId:userId,
     persona:persona
   })
 
   return response.data.data.choices[0].message.content
 }
+
